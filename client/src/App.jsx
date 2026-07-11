b1a373e9cafe63880a6de6f45eb9aa113e43e021
Zo Vergel de Dios <186210224+zo-vergeldedios@users.noreply.github.com>
2026-07-10 18:36:47 -0700

    Fixed the placement of components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,17 +2,214 @@ import { useState, useEffect } from "react";
 import fitDexLogo from "./assets/fitDex-logo.png";
 // import "./App.css";
 
-// let count = ""
+function ShowErrorOrSuccessMessage({ message }) {
+  return <span id="successAndErrorMessage">{message}</span>;
+}
+
+function EditWorkout({
+  workout,
+  workouts,
+  setWorkouts,
+  setSuccessAndErrorMessage,
+  weekdayIndex,
+}) {
+  const [updatedWorkout, setUpdatedWorkout] = useState(workout.workout_name);
+  const [updatedReps, setUpdatedReps] = useState(workout.reps);
+  const [updatedSets, setUpdatedSets] = useState(workout.sets);
+  const [updatedWeight, setUpdatedWeight] = useState(workout.weight);
+  // {
+  // useEffect(() => {
+  // setNewWorkout(workout.workout_name);
+  // setNewReps(workout.reps);
+  // setNewSets(workout.sets);
+  // setNewWeight(workout.weight);
+  // }, [workouts]);
+  // }
+  return (
+    <li>
+      <input
+        id="workout-name-input"
+        type="text"
+        onChange={(e) => setUpdatedWorkout(e.target.value)}
+        className="li-info li-workout-name"
+        value={updatedWorkout}
+      />
+      <input
+        type="text"
+        onChange={(e) => setUpdatedSets(e.target.value)}
+        className="li-info"
+        value={updatedSets}
+      />
+      <input
+        type="text"
+        onChange={(e) => setUpdatedReps(e.target.value)}
+        className="li-info"
+        value={updatedReps}
+      />
+      <input
+        type="text"
+        onChange={(e) => setUpdatedWeight(e.target.value)}
+        className="li-info"
+        value={updatedWeight}
+      />
+      <button
+        className="saveAndDeleteButton"
+        onClick={() => {
+          setWorkouts(workouts.filter((wo) => wo.id !== workout.id));
+          fetch(`${import.meta.env.VITE_API_URL}/delete`, {
+            method: "POST",
+            mode: "cors",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              id: workout.id,
+            }),
+          })
+            .then((res) => {
+              console.log({ success: "workout successfully deleted" });
+              setSuccessAndErrorMessage("Workout deleted");
+              return res.json;
+              // TODO: success message
+            })
+            .catch(function (error) {
+              console.log(error);
+              setSuccessAndErrorMessage("Failed to delete the workout");
+              // TODO: failure message
+            });
+        }}
+      >
+        ❌
+      </button>
+      <button
+        className="saveAndDeleteButton"
+        onClick={() => {
+          setWorkouts(
+            workouts.map((wo) => {
+              if (workout.id == wo.id) {
+                wo.workout_name = updatedWorkout;
+                wo.sets = updatedSets;
+                wo.reps = updatedReps;
+                wo.weight = updatedWeight;
+                wo.editing = false;
+                wo.weekday_index = weekdayIndex;
+              }
+              return wo;
+            }),
+          );
+          fetch(`${import.meta.env.VITE_API_URL}/update`, {
+            method: "POST",
+            mode: "cors",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              id: workout.id,
+              workout_name: updatedWorkout,
+              sets: updatedSets,
+              reps: updatedReps,
+              weight: updatedWeight,
+              editing: false,
+              weekday_index: weekdayIndex,
+            }),
+          })
+            .then((res) => {
+              setSuccessAndErrorMessage("Workout updated");
+              console.log({ success: "workout updated" });
+              return res.json;
+            })
+            .then((data) => {
+              console.log(data);
+            })
+            .catch(function (error) {
+              setSuccessAndErrorMessage("Failed to update the workout");
+              console.log(error);
+            });
+        }}
+      >
+        ✅
+      </button>
+    </li>
+  );
+}
+
+function ShowProgram({
+  wo,
+  workouts,
+  setWorkouts,
+  setSuccessAndErrorMessage,
+  weekdayIndex,
+}) {
+  // const [workouts, setWorkouts] = useState([]);
+
+  return (
+    <div className="workout-div">
+      <span className="workout-title">{wo.workout_name}</span>
+      <span className="workout-title">{wo.sets}</span>
+      <span className="workout-title">{wo.reps}</span>
+      <span className="workout-title">{wo.weight}</span>
+      <button
+        className="edit-button"
+        onClick={() => {
+          setWorkouts(
+            workouts.map((wo2) => {
+              if (wo2.id == wo.id) {
+                wo2.editing = true;
+              }
+              return wo2;
+            }),
+          );
+
+          console.log(wo, "edit program");
+
+          fetch(`${import.meta.env.VITE_API_URL}/update`, {
+            method: "POST",
+            mode: "cors",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              id: wo.id,
+              workout_name: wo.workout_name,
+              sets: wo.sets,
+              reps: wo.reps,
+              weight: wo.weight,
+              editing: true,
+              weekday_index: weekdayIndex,
+            }),
+          })
+            .then((res) => {
+              setSuccessAndErrorMessage("Workout ready for edit");
+              console.log({ success: "workout ready for edit" });
+              return res.json;
+            })
+            .then((data) => {
+              console.log(data);
+            })
+            .catch(function (error) {
+              setSuccessAndErrorMessage("Workout NOT ready for edit");
+              console.log(error);
+            });
+        }}
+      >
+        Edit
+      </button>
+    </div>
+    // console.log("check")
+  );
+}
+
 function App() {
-  const [workouts, setWorkouts] = useState([]);
-  const [newWorkout, setNewWorkout] = useState("");
-  const [newReps, setNewReps] = useState("");
-  const [newSets, setNewSets] = useState("");
-  const [newWeight, setNewWeight] = useState("");
   // const [error, setError] = useState("");
   // const [userHasHoverButton, setUserHasHoverButton] = useState(false);
 
   // const [selectedDay, setSelectedDay] = useState(0);
+  const [newWorkout, setNewWorkout] = useState("");
+  const [newReps, setNewReps] = useState("");
+  const [newSets, setNewSets] = useState("");
+  const [newWeight, setNewWeight] = useState("");
+  const [workouts, setWorkouts] = useState([]);
+  const [successAndErrorMessage, setSuccessAndErrorMessage] = useState("");
 
   const weekdays = [
     "Sunday",
@@ -26,185 +223,17 @@ function App() {
 
   const [weekdayIndex, setWeekdayIndex] = useState(new Date().getDay());
 
-  const [successAndErrorMessage, setSuccessAndErrorMessage] = useState("");
+  // const [hiddenButton, setHiddenButton] = useState(true);
+  // const hiddenButton = workouts.filter((wo) => wo.editing == true).length === 0;
+  // const hiddenButton = workouts.every((wo) => !wo.editing);
 
-  function ShowErrorOrSuccessMessage() {
-    useEffect(() => {
-      const message = setTimeout(() => {
-        setSuccessAndErrorMessage("");
-      }, 3000);
-      return () => clearTimeout(message);
-    }, []);
-
-    return <span id="successAndErrorMessage">{successAndErrorMessage}</span>;
-  }
-
-  function EditWorkout({ workout }) {
-    const [newWorkout, setNewWorkout] = useState(workout.workout_name);
-    const [newReps, setNewReps] = useState(workout.reps);
-    const [newSets, setNewSets] = useState(workout.sets);
-    const [newWeight, setNewWeight] = useState(workout.weight);
-    return (
-      <li>
-        <input
-          id="workout-name-input"
-          type="text"
-          onChange={(e) => setNewWorkout(e.target.value)}
-          className="li-info li-workout-name"
-          value={newWorkout}
-        />
-        <input
-          type="text"
-          onChange={(e) => setNewSets(e.target.value)}
-          className="li-info"
-          value={newSets}
-        />
-        <input
-          type="text"
-          onChange={(e) => setNewReps(e.target.value)}
-          className="li-info"
-          value={newReps}
-        />
-        <input
-          type="text"
-          onChange={(e) => setNewWeight(e.target.value)}
-          className="li-info"
-          value={newWeight}
-        />
-        <button
-          className="saveAndDeleteButton"
-          onClick={() => {
-            setWorkouts(workouts.filter((wo) => wo.id !== workout.id));
-            fetch(`${import.meta.env.VITE_API_URL}/delete`, {
-              method: "POST",
-              mode: "cors",
-              headers: {
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify({
-                id: workout.id,
-              }),
-            })
-              .then((res) => {
-                console.log({ success: "workout successfully deleted" });
-                setSuccessAndErrorMessage("Workout deleted");
-                return res.json;
-                // TODO: success message
-              })
-              .catch(function (error) {
-                console.log(error);
-                setSuccessAndErrorMessage("Failed to delete the workout");
-                // TODO: failure message
-              });
-          }}
-        >
-          ❌
-        </button>
-        <button
-          className="saveAndDeleteButton"
-          onClick={() => {
-            setWorkouts(
-              workouts.map((wo) => {
-                if (workout.id == wo.id) {
-                  wo.workout_name = newWorkout;
-                  wo.sets = newSets;
-                  wo.reps = newReps;
-                  wo.weight = newWeight;
-                  wo.editing = false;
-                  wo.weekday_index = weekdayIndex;
-                }
-                return wo;
-              }),
-            );
-            fetch(`${import.meta.env.VITE_API_URL}/update`, {
-              method: "POST",
-              mode: "cors",
-              headers: {
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify({
-                id: workout.id,
-                workout_name: newWorkout,
-                sets: newSets,
-                reps: newReps,
-                weight: newWeight,
-                editing: false,
-                weekday_index: weekdayIndex,
-              }),
-            })
-              .then((res) => {
-                setSuccessAndErrorMessage("Workout updated");
-                console.log({ success: "workout updated" });
-                return res.json;
-              })
-              .then((data) => {
-                console.log(data);
-              })
-              .catch(function (error) {
-                setSuccessAndErrorMessage("Failed to update the workout");
-                console.log(error);
-              });
-          }}
-        >
-          ✅
-        </button>
-      </li>
-    );
-  }
-  function ShowProgram({ wo }) {
-    return (
-      <div className="workout-div">
-        <span className="workout-title">{wo.workout_name}</span>
-        <span className="workout-title">{wo.sets}</span>
-        <span className="workout-title">{wo.reps}</span>
-        <span className="workout-title">{wo.weight}</span>
-        <button
-          className="edit-button"
-          onClick={() => {
-            setWorkouts(
-              workouts.map((wo2) => {
-                if (wo2.id == wo.id) {
-                  wo2.editing = true;
-                }
-                return wo2;
-              }),
-            );
-
-            console.log(wo, "edit program");
-
-            fetch(`${import.meta.env.VITE_API_URL}/update`, {
-              method: "POST",
-              mode: "cors",
-              headers: {
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify({
-                id: wo.id,
-                workout_name: wo.workout_name,
-                sets: wo.sets,
-                reps: wo.reps,
-                weight: wo.weight,
-                editing: true,
-                weekday_index: weekdayIndex,
-              }),
-            })
-              .then((res) => {
-                return res.json;
-              })
-              .then((data) => {
-                console.log(data);
-              })
-              .catch(function (error) {
-                console.log(error);
-              });
-          }}
-        >
-          Edit
-        </button>
-      </div>
-      // console.log("check")
-    );
-  }
+  useEffect(() => {
+    const timeoutID = setTimeout(() => {
+      setSuccessAndErrorMessage("");
+    }, 3000);
+    return () => clearTimeout(timeoutID);
+  }, [successAndErrorMessage]);
+  // TODO - Get all of the components out of App().
 
   useEffect(() => {
     fetch(`${import.meta.env.VITE_API_URL}/list`)
@@ -259,12 +288,63 @@ function App() {
           <span className="workout-title-heading">Sets</span>
           <span className="workout-title-heading">Reps</span>
           <span className="workout-title-heading">Weights</span>
+          <button
+            className="edit-all-button"
+            onClick={() => {
+              setWorkouts(
+                workouts.map((wo) => {
+                  if (weekdayIndex == wo.weekday_index) {
+                    wo.editing = true;
+
+                    fetch(`${import.meta.env.VITE_API_URL}/update`, {
+                      method: "POST",
+                      mode: "cors",
+                      headers: {
+                        "Content-Type": "application/json",
+                      },
+                      body: JSON.stringify({
+                        id: wo.id,
+                        workout_name: wo.workout_name,
+                        sets: wo.sets,
+                        reps: wo.reps,
+                        weight: wo.weight,
+                        editing: true,
+                        weekday_index: weekdayIndex,
+                      }),
+                    })
+                      .then((res) => {
+                        return res.json;
+                      })
+                      .then((data) => {
+                        console.log(data);
+                      })
+                      .catch(function (error) {
+                        setSuccessAndErrorMessage(
+                          "Workouts NOT ready for edit",
+                        );
+                        console.log(error);
+                      });
+                  }
+                  return wo;
+                }),
+              );
+            }}
+          >
+            Edit All
+          </button>
         </div>
         <ul id="workoutplan-container">
           {workouts
             .filter((wo) => wo.weekday_index === weekdayIndex && !wo.editing)
             .map((wo) => (
-              <ShowProgram wo={wo} key={wo.id} />
+              <ShowProgram
+                wo={wo}
+                key={wo.id}
+                workouts={workouts}
+                setWorkouts={setWorkouts}
+                setSuccessAndErrorMessage={setSuccessAndErrorMessage}
+                weekdayIndex={weekdayIndex}
+              />
             ))}
         </ul>
       </div>
@@ -297,7 +377,6 @@ function App() {
           value={newWorkout}
           id="workout-input"
           onChange={(e) => {
-            // console.log({ e });
             setNewWorkout(e.target.value);
           }}
         />
@@ -308,7 +387,6 @@ function App() {
           type="number"
           id="sets-input"
           onChange={(e) => {
-            // console.log({ e });
             setNewSets(Number(e.target.value));
           }}
         />
@@ -319,7 +397,6 @@ function App() {
           id="reps-input"
           type="number"
           onChange={(e) => {
-            // console.log({ e });
             setNewReps(Number(e.target.value));
           }}
         />
@@ -330,14 +407,11 @@ function App() {
           id="weight-input"
           type="number"
           onChange={(e) => {
-            // console.log({ e });
             setNewWeight(Number(e.target.value));
           }}
         />
       </div>
-      {/* TODO - Fix the messages */}
-      {/* {inputError && userHasHoverButton && <span id="error">{inputError}</span>} */}
-      <ShowErrorOrSuccessMessage />
+      <ShowErrorOrSuccessMessage message={successAndErrorMessage} />
       <button
         id="add-workout-button"
         className={isButtonDisabled ? "disabled" : ""}
@@ -407,9 +481,61 @@ function App() {
           {workouts
             .filter((wo) => wo.editing)
             .map((workout) => (
-              <EditWorkout workout={workout} key={workout.id} />
+              <EditWorkout
+                workout={workout}
+                key={workout.id}
+                workouts={workouts}
+                setWorkouts={setWorkouts}
+                setSuccessAndErrorMessage={setSuccessAndErrorMessage}
+                weekdayIndex={weekdayIndex}
+              />
             ))}
         </ul>
+
+        <button
+          className="save-all-button"
+          hidden={workouts.every((wo) => !wo.editing)}
+          onClick={() => {
+            setWorkouts(
+              workouts.map((wo) => {
+                if (wo.editing == true) {
+                  wo.editing = false;
+                  console.log(wo);
+
+                  fetch(`${import.meta.env.VITE_API_URL}/update`, {
+                    method: "POST",
+                    mode: "cors",
+                    headers: {
+                      "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                      id: wo.id,
+                      workout_name: wo.workout_name,
+                      sets: wo.sets,
+                      reps: wo.reps,
+                      weight: wo.weight,
+                      editing: false,
+                      weekday_index: weekdayIndex,
+                    }),
+                  })
+                    .then((res) => {
+                      return res.json;
+                    })
+                    .then((data) => {
+                      console.log(data);
+                    })
+                    .catch(function (error) {
+                      setSuccessAndErrorMessage("Workouts NOT ready for edit");
+                      console.log(error);
+                    });
+                }
+                return wo;
+              }),
+            );
+          }}
+        >
+          Save All Workouts
+        </button>
       </div>
     </main>
   );
